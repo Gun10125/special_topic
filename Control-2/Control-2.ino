5daa0edb33260067a2035e0aab717a03269bbf85
MRARE66C <tvsupergame@gmail.com>
2024-08-12 14:42:10 +0700

    Added comment to the code

--- Control-2/Control-2.ino
+++ Control-2/Control-2.ino
@@ -7,14 +7,20 @@
 #define MT_POT  A0
 #define V_POT   A3
 
+// Variables to store motor and potentiometer values
 signed int motor_val = 0;
 signed int pot_read = 0;
 
+// Variable to count LED toggles
 volatile unsigned int led_count = 0;
 
+// Timer object
 hw_timer_t *My_timer = NULL;
+
+// Interrupt Service Routine (ISR) for the timer
 void IRAM_ATTR onTimer()
 {
+  // Increment led_count and toggle LED1 every 1000 counts (1 second)
   if((++led_count) > 1000)
   {
     led_count = 0;
@@ -24,17 +30,18 @@ void IRAM_ATTR onTimer()
 
 void setup()
 {
-  pinMode(LED1,OUTPUT); //LED1
-  pinMode(LED2,OUTPUT); //LED2
-  pinMode(BUTTON1,INPUT);  //BUTTON1
-  pinMode(BUTTON2,INPUT);  //BUTTON2  
-  pinMode(MT_IN1,OUTPUT);
-  pinMode(MT_IN2,OUTPUT); 
+  // Set pin modes for LEDs and buttons
+  pinMode(LED1,OUTPUT); // LED1
+  pinMode(LED2,OUTPUT); // LED2
+  pinMode(BUTTON1,INPUT);  // BUTTON1
+  pinMode(BUTTON2,INPUT);  // BUTTON2  
+  pinMode(MT_IN1,OUTPUT);  // Motor input 1
+  pinMode(MT_IN2,OUTPUT);  // Motor input 2
 
-  My_timer = timerBegin(0, 80, true);  // use tiemr 0 and set prescale to 80 so 1 tick is 1 uSec
-  timerAttachInterrupt(My_timer, &onTimer, true); // point to the ISR
-  timerAlarmWrite(My_timer, 1000, true);  // set alarm every 1 ms
-  timerAlarmEnable(My_timer);  // enable the alarm
+  // Initialize timer
+  My_timer = timerBegin(0, 80, true);  // Use timer 0 and set prescale to 80 so 1 tick is 1 microsecond
+  timerAttachInterrupt(My_timer, &onTimer, true); // Attach the ISR to the timer
+  timerAlarmWrite(My_timer, 1000, true);  // Set alarm to trigger every 1 millisecond
 }
 
 void loop()
